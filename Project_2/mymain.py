305511afe79655b75611533173ef16888bf31bca
koric1@illinois.edu <koric1@illinois.edu>
2020-10-30 16:27:49 -0500

    testing values

--- Project_2/mymain.py
+++ Project_2/mymain.py
@@ -2,15 +2,17 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
+import warnings
+from sklearn.exceptions import DataConversionWarning
+from sklearn.ensemble import GradientBoostingRegressor
+warnings.filterwarnings(action='ignore', category=DataConversionWarning)
 
-def december(holiday,week):
-    day = 0
-    if holiday == 1 or week == 51 or week == 52 or week == 53:
-        day = 1
-        
-    return day
 
-def mypredict(train, test, new_test, t):
+
+
+def mypredict(train, test, new_test, t,i):
+    
+    
     df = pd.concat([train, new_test], join="inner",ignore_index = True)
     df['month'] = pd.DatetimeIndex(df['Date']).month
     df['year'] = pd.DatetimeIndex(df['Date']).year
@@ -19,18 +21,21 @@ def mypredict(train, test, new_test, t):
 
     
 
-    df['holiday'] = df.apply(lambda x: december(x.holiday, x.week), axis=1)
-    
-
     X = df[['Store','Dept','week','holiday','year']].values
     y = df[['Weekly_Sales']].values
-    reg = RandomForestRegressor(n_estimators=58, max_depth=27, max_features=4, min_samples_split=3, min_samples_leaf=1).fit(X, y)
+    
+    
+    reg = RandomForestRegressor(n_estimators=57, max_depth=i, min_samples_split=3, random_state = 0).fit(X, y)
+    #reg = LinearRegression().fit(X,y)
+    #reg = GradientBoostingRegressor(n_estimators=57, max_depth=27, max_features=5, min_samples_split=3, min_samples_leaf=1, random_state = 0).fit(X, y)
+    
+    
     test["holiday"] = test['IsHoliday'].apply(lambda is_holiday:1 if is_holiday else 0)
     test['month'] = pd.DatetimeIndex(test['Date']).month
     test['year'] = pd.DatetimeIndex(test['Date']).year
     test['week'] = test['Date'].apply(lambda x: x.week)
 
-    test['holiday'] = test.apply(lambda x: december(x.holiday, x.week), axis=1)
+    
 
     X_test = test[['Store','Dept','week','holiday','year']].values
     y_pred = reg.predict(X_test)
---
+++ Project_2/score_real.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+
+from mymain import mypredict
+
+train = pd.read_csv('train_ini.csv', parse_dates=['Date'])
+test = pd.read_csv('test.csv', parse_dates=['Date'])
+
+# save weighed mean absolute error WMAE
+n_folds = 10
+next_fold = None
+wae = []
+# time-series CV
+for t in range(1, n_folds+1):
+    print(t)
+    # *** THIS IS YOUR PREDICTION FUNCTION ***
+    train, test = mypredict(train, test, next_fold, t)
+
+    # Load fold file
+    # You should add this to your training data in the next call to mypredict()
+    fold_file = 'fold_{t}.csv'.format(t=t)
+    next_fold = pd.read_csv(fold_file, parse_dates=['Date'])
+
+    # extract predictions matching up to the current fold
+    scoring_df = next_fold.merge(test, on=['Date', 'Store', 'Dept'], how='left')
+
+    # extract weights and convert to numpy arrays for wae calculation
+    weights = scoring_df['IsHoliday_x'].apply(lambda is_holiday:5 if is_holiday else 1).to_numpy()
+    actuals = scoring_df['Weekly_Sales'].to_numpy()
+    preds = scoring_df['Weekly_Pred'].fillna(0).to_numpy()
+
+    wae.append((np.sum(weights * np.abs(actuals - preds)) / np.sum(weights)).item())
+
+print(wae)
+print(sum(wae)/len(wae))
